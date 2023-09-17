98b9ab7ff6119e0573d0a8be331ff6ea6cadf374
aybeedee <abdullahumer575@gmail.com>
2023-09-17 18:02:58 +0500

    backed up latest placement script

---
+++ script-backups/PlacementController2.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+//automatically adds ray cast manager when script is added to a game object
+[RequireComponent(typeof(ARRaycastManager))]
+
+public class PlacementController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject placedPrefab;
+
+    private ARRaycastManager arRaycastManager;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private List<GameObject> addedPrefabs = new List<GameObject>();
+
+    private Vector2 touchPosition = default;
+
+    public GameObject PlacedPrefab {
+
+        get { 
+
+            return placedPrefab;
+        }
+
+        set {
+
+            placedPrefab = value;
+        }
+    }
+
+    void Awake() {
+
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update() {
+
+        if (Input.touchCount > 0) { 
+        
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began) {
+
+                touchPosition = touch.position;
+
+                if (arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon)) {
+
+                    var hitPose = hits[0].pose;
+
+                    if (addedPrefabs.Count < 2) {
+
+                        GameObject addedPrefab = Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+                        addedPrefabs.Add(addedPrefab);
+                    }
+                }
+            }
+
+            if (touch.phase == TouchPhase.Moved) {
+
+                touchPosition = touch.position;
+
+                if (arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon)) {
+
+                    var hitPose = hits[0].pose;
+
+                    if (addedPrefabs.Count > 0) {
+
+                        GameObject lastAddedPrefab = addedPrefabs[addedPrefabs.Count - 1];
+                        lastAddedPrefab.transform.position = hitPose.position;
+                        lastAddedPrefab.transform.rotation = hitPose.rotation;
+                    }
+                }
+            }
+        }
+    }
+}
